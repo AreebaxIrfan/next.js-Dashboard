8e66e61debe62622c4b8b19a3d5c86b3281f7ab4
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-02 01:09:51 +0500

    edite-3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { z } from 'zod';
+import { any, z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -36,42 +36,45 @@ export type State = {
   message?: string | null;
 };
 export async function createInvoice(prevState: State, formData: FormData) {
-  const validatedFields = UpdateInvoice.safeParse({
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
-
-
-
+ 
+  // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
+ 
   // Prepare data for insertion into the database
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
+ 
+  // Insert data into the database
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
-
+ 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
@@ -80,6 +83,9 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
+
+
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -96,6 +102,8 @@ const FormSchema = z.object({
 });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+
 
 export async function updateInvoice(
   id: string,
